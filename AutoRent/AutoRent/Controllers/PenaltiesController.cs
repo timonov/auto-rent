17cfb3bd12204410b1978e2bf73b6f4d630d5169
K. Nikolaev <nikolaevk.95@gmail.com>
2017-04-17 22:12:50 +0300

    added ctrls and views

---
+++ AutoRent/AutoRent/Controllers/PenaltiesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoRent.Models;
+
+namespace AutoRent.Controllers
+{
+    public class PenaltiesController : Controller
+    {
+        private AutoRentContext db = new AutoRentContext();
+
+        // GET: Penalties
+        public ActionResult Index()
+        {
+            return View(db.Penalties.ToList());
+        }
+
+        // GET: Penalties/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Penalty penalty = db.Penalties.Find(id);
+            if (penalty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(penalty);
+        }
+
+        // GET: Penalties/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Penalties/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,type,amount")] Penalty penalty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Penalties.Add(penalty);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(penalty);
+        }
+
+        // GET: Penalties/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Penalty penalty = db.Penalties.Find(id);
+            if (penalty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(penalty);
+        }
+
+        // POST: Penalties/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,type,amount")] Penalty penalty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(penalty).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(penalty);
+        }
+
+        // GET: Penalties/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Penalty penalty = db.Penalties.Find(id);
+            if (penalty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(penalty);
+        }
+
+        // POST: Penalties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Penalty penalty = db.Penalties.Find(id);
+            db.Penalties.Remove(penalty);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AutoRent/AutoRent/Controllers/RentsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoRent.Models;
+
+namespace AutoRent.Controllers
+{
+    public class RentsController : Controller
+    {
+        private AutoRentContext db = new AutoRentContext();
+
+        // GET: Rents
+        public ActionResult Index()
+        {
+            var rents = db.Rents.Include(r => r.car).Include(r => r.customer).Include(r => r.customerFavour);
+            return View(rents.ToList());
+        }
+
+        // GET: Rents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rent rent = db.Rents.Find(id);
+            if (rent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rent);
+        }
+
+        // GET: Rents/Create
+        public ActionResult Create()
+        {
+            ViewBag.CarID = new SelectList(db.Cars, "ID", "brand");
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "firstName");
+            ViewBag.CustomerFavourID = new SelectList(db.CustomerFavours, "ID", "favouriteBrand");
+            return View();
+        }
+
+        // POST: Rents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,CarID,CustomerID,CustomerFavourID,dateOfService,dateOfReturn")] Rent rent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rents.Add(rent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CarID = new SelectList(db.Cars, "ID", "brand", rent.CarID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "firstName", rent.CustomerID);
+            ViewBag.CustomerFavourID = new SelectList(db.CustomerFavours, "ID", "favouriteBrand", rent.CustomerFavourID);
+            return View(rent);
+        }
+
+        // GET: Rents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rent rent = db.Rents.Find(id);
+            if (rent == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CarID = new SelectList(db.Cars, "ID", "brand", rent.CarID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "firstName", rent.CustomerID);
+            ViewBag.CustomerFavourID = new SelectList(db.CustomerFavours, "ID", "favouriteBrand", rent.CustomerFavourID);
+            return View(rent);
+        }
+
+        // POST: Rents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CarID,CustomerID,CustomerFavourID,dateOfService,dateOfReturn")] Rent rent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CarID = new SelectList(db.Cars, "ID", "brand", rent.CarID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "firstName", rent.CustomerID);
+            ViewBag.CustomerFavourID = new SelectList(db.CustomerFavours, "ID", "favouriteBrand", rent.CustomerFavourID);
+            return View(rent);
+        }
+
+        // GET: Rents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rent rent = db.Rents.Find(id);
+            if (rent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rent);
+        }
+
+        // POST: Rents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rent rent = db.Rents.Find(id);
+            db.Rents.Remove(rent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
